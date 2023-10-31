2efe56b7dd1d4e1fd04e6263224409b3415e8b33
Mathieu52 <Mathieu52@users.noreply.github.com>
2023-10-31 15:11:09 +0000

    deploy: a8b4d064cdad658d4a3f2bcf319d7f3193ceb1e1

--- search/all_d.js
+++ search/all_d.js
@@ -1,7 +1,11 @@
 var searchData=
 [
-  ['showdatapid_0',['showDataPID',['../db/d41/namespaceMOVE.html#a315e036cd760b1e3d9320732fc4f7137',1,'MOVE']]],
-  ['sp_1',['Sp',['../dd/d4f/structMOVE_1_1valeursPID.html#a3ad75d15a1ca1e6537523b01ae5299a6',1,'MOVE::valeursPID::Sp()'],['../de/db6/structPID_1_1valeursPID.html#a677c7489f519ee4d41ff1346a9a66ab5',1,'PID::valeursPID::Sp()']]],
-  ['speedtovoltage_2',['speedToVoltage',['../db/d41/namespaceMOVE.html#a6dc94d29c95a22759d8046d06361f99e',1,'MOVE']]],
-  ['startpos_3',['startPos',['../db/d41/namespaceMOVE.html#a713957715c2c4f0f8922dd3630098a77',1,'MOVE']]]
+  ['setleftspeed_0',['setLeftSpeed',['../db/db7/namespaceMovement.html#ae5164bcc0af81eb0fa2acf092e120f77',1,'Movement']]],
+  ['setpidleft_1',['setPIDLeft',['../db/db7/namespaceMovement.html#a7144c384519feecfb9a0f03317d90d21',1,'Movement']]],
+  ['setpidright_2',['setPIDRight',['../db/db7/namespaceMovement.html#ae9b896b37ab3dd44d8ea9d303cf06af4',1,'Movement']]],
+  ['setrightspeed_3',['setRightSpeed',['../db/db7/namespaceMovement.html#a8a047da605804b001fe16200838f853c',1,'Movement']]],
+  ['showdatapid_4',['showDataPID',['../db/d41/namespaceMOVE.html#a315e036cd760b1e3d9320732fc4f7137',1,'MOVE']]],
+  ['sp_5',['Sp',['../dd/d4f/structMOVE_1_1valeursPID.html#a3ad75d15a1ca1e6537523b01ae5299a6',1,'MOVE::valeursPID::Sp()'],['../de/db6/structPID_1_1valeursPID.html#a677c7489f519ee4d41ff1346a9a66ab5',1,'PID::valeursPID::Sp()']]],
+  ['speedtovoltage_6',['speedToVoltage',['../db/d41/namespaceMOVE.html#a6dc94d29c95a22759d8046d06361f99e',1,'MOVE']]],
+  ['startpos_7',['startPos',['../db/d41/namespaceMOVE.html#a713957715c2c4f0f8922dd3630098a77',1,'MOVE']]]
 ];
--- search/functions_b.js
+++ search/functions_b.js
@@ -1,5 +1,9 @@
 var searchData=
 [
-  ['showdatapid_0',['showDataPID',['../db/d41/namespaceMOVE.html#a315e036cd760b1e3d9320732fc4f7137',1,'MOVE']]],
-  ['speedtovoltage_1',['speedToVoltage',['../db/d41/namespaceMOVE.html#a6dc94d29c95a22759d8046d06361f99e',1,'MOVE']]]
+  ['setleftspeed_0',['setLeftSpeed',['../db/db7/namespaceMovement.html#ae5164bcc0af81eb0fa2acf092e120f77',1,'Movement']]],
+  ['setpidleft_1',['setPIDLeft',['../db/db7/namespaceMovement.html#a7144c384519feecfb9a0f03317d90d21',1,'Movement']]],
+  ['setpidright_2',['setPIDRight',['../db/db7/namespaceMovement.html#ae9b896b37ab3dd44d8ea9d303cf06af4',1,'Movement']]],
+  ['setrightspeed_3',['setRightSpeed',['../db/db7/namespaceMovement.html#a8a047da605804b001fe16200838f853c',1,'Movement']]],
+  ['showdatapid_4',['showDataPID',['../db/d41/namespaceMOVE.html#a315e036cd760b1e3d9320732fc4f7137',1,'MOVE']]],
+  ['speedtovoltage_5',['speedToVoltage',['../db/d41/namespaceMOVE.html#a6dc94d29c95a22759d8046d06361f99e',1,'MOVE']]]
 ];
