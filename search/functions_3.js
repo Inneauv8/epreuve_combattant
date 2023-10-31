29797d5d748ccc6303959c01e9c71c0ce075e783
Mathieu52 <Mathieu52@users.noreply.github.com>
2023-10-31 20:19:46 +0000

    deploy: 0e3f5efabe19195e832c6c234edc464d8bead8ac

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['forward_0',['forward',['../db/db7/namespaceMovement.html#a0aee57f6a368fc7b6048764a25224f42',1,'Movement']]]
+  ['forward_0',['forward',['../db/db7/namespaceMovement.html#a53c94a82981a4b350d5b1803c4b9a1fd',1,'Movement']]]
 ];
--- search/functions_a.js
+++ search/functions_a.js
@@ -5,5 +5,5 @@ var searchData=
   ['radiustospeedg_2',['radiusToSpeedG',['../db/d41/namespaceMOVE.html#afa3858ba090b6dcda39c2f9bebfe2673',1,'MOVE']]],
   ['readlinevalue_3',['readLineValue',['../d5/d4d/namespaceCapteurLigne.html#a9f1e7930d15aa89cb90dda262db91a2e',1,'CapteurLigne']]],
   ['resetorientation_4',['resetOrientation',['../db/db7/namespaceMovement.html#a50d91a42185c3f8034ab78fd590f8720',1,'Movement']]],
-  ['rotate_5',['rotate',['../db/db7/namespaceMovement.html#aa1d15801d635b678ea08ac18a1cef661',1,'Movement::rotate(float velocity, float radius)'],['../db/db7/namespaceMovement.html#af45ab3090fa01010d6aef30d67d09484',1,'Movement::rotate(float velocity, float radius, float angle)']]]
+  ['rotate_5',['rotate',['../db/db7/namespaceMovement.html#aa1d15801d635b678ea08ac18a1cef661',1,'Movement::rotate(float velocity, float radius)'],['../db/db7/namespaceMovement.html#a170c79f5d9f9d48af9ef9eb0e347a9f6',1,'Movement::rotate(float velocity, float radius, float angle, boolean reset)']]]
 ];
