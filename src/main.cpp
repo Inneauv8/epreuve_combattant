062f680951c824dc7586db380033fb3854eaeb27
Samuel Hamelin <samuelh1103@hotmail.com>
2023-10-31 11:11:11 -0400

    just a smol push uwu~ nya

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,25 @@ Auteur : Maxime Boucher
 Date de création : 2023-10-17
 
 Description : Librairie permettant la mise en application du capteur de ligne
-              
-Notes : 
 
-Modifications : 
+Notes :
+
+Modifications :
 
 ***************************************************************************************************/
 
 // *************************************************************************************************
 //  INCLUDES
-// *************************************************************************************************	
+// *************************************************************************************************
 
 #include "capteurLigne.h"
 #include "PIDLigne.h"
 #include "Move.h"
+#include "sifflet.h"
 #include <LibRobus.h>
 #include <float.h>
 #include <mathX.h>
 #include <Adafruit_TCS34725.h>
-
 
 // *************************************************************************************************
 //  CONSTANTES
@@ -38,12 +38,11 @@ int movementIndex = -1;
 //  FONCTIONS LOCALES
 // *************************************************************************************************
 
-
 // *************************************************************************************************
 //  STRUCTURES ET UNIONS
 // *************************************************************************************************
 
-enum STATE 
+enum STATE
 {
     Bleu = 0,
     Vert = 1,
@@ -51,12 +50,14 @@ enum STATE
     Rouge = 3,
 };
 
-enum ClawState {
+enum ClawState
+{
     OPENED,
     CLOSED,
 };
 
-enum ArmState {
+enum ArmState
+{
     EXTENDED_RIGHT,
     NOT_EXTENDED,
     EXTENDED_LEFT,
@@ -70,11 +71,13 @@ ArmState armState = NOT_EXTENDED;
 ClawState clawState = OPENED;
 
 long initialTime = 0;
+byte state = 0;
 
 MOVE::valeursPID rightPID = {};
 MOVE::valeursPID leftPID = {};
 
-void setupPID() {
+void setupPID()
+{
     rightPID.Kp = 0.0625;
     rightPID.Ki = 0.0001;
     rightPID.Kd = 0.001;
@@ -84,94 +87,109 @@ void setupPID() {
     leftPID.Kd = 0.001;
 }
 
-void setupServo() {
+void setupServo()
+{
     SERVO_Enable(ARM_SERVO);
     SERVO_SetAngle(ARM_SERVO, 90);
     SERVO_Enable(CLAW_SERVO);
 }
 
 void setup()
-{   
+{
     setupPID();
     setupServo();
     BoardInit();
-    //Good value : 8, 0.001, 0.15
-    //PIDLigne::initPID(2.7387791339, 2.625137795, 6, 0, 0.1, LINE_FOLLOWER_PINS, 45);
+    // Good value : 8, 0.001, 0.15
+    // PIDLigne::initPID(2.7387791339, 2.625137795, 6, 0, 0.1, LINE_FOLLOWER_PINS, 45);
     Serial.begin(9600);
 
     ENCODER_Reset(RIGHT);
     ENCODER_Reset(LEFT);
 
     initialTime = millis();
+    Sifflet::init();
 }
 
-float computeOrientation() {
+float computeOrientation()
+{
     float deltaS = (ENCODER_Read(LEFT) - ENCODER_Read(RIGHT)) * MOVE::pulseToDist / 2.0;
     float theta = deltaS * 2 / (WHEEL_BASE_DIAMETER);
 
     return theta;
 }
 
-float computeDistance() {
+float computeDistance()
+{
     float distance = (ENCODER_Read(LEFT) + ENCODER_Read(RIGHT)) * MOVE::pulseToDist / 2.0;
-    
+
     return distance;
 }
 
-float radToDeg(float rad) {
+float radToDeg(float rad)
+{
     return rad * 180 / M_PI;
 }
 
-bool distanceFlag(float distance, float *initialDistance) {
+bool distanceFlag(float distance, float *initialDistance)
+{
     float actualDistance = computeDistance();
 
-    if (isnan(*initialDistance)) {
+    if (isnan(*initialDistance))
+    {
         *initialDistance = actualDistance;
     }
 
     bool distanceReached = fabs(actualDistance - *initialDistance) >= fabs(distance);
-    
-    if (distanceReached) {
+
+    if (distanceReached)
+    {
         *initialDistance = NAN;
     }
 
     return distanceReached;
 }
 
-bool orientationFlag(float angle, float *initialOrientation) {
+bool orientationFlag(float angle, float *initialOrientation)
+{
     float actualOrientation = computeOrientation();
 
-    if (isnan(*initialOrientation)) {
+    if (isnan(*initialOrientation))
+    {
         *initialOrientation = actualOrientation;
     }
 
     bool angleReached = fabs(actualOrientation - *initialOrientation) >= fabs(angle);
-    
-    if (angleReached) {
+
+    if (angleReached)
+    {
         *initialOrientation = NAN;
     }
 
     return angleReached;
 }
 
-bool distanceFlag(float distance) {
+bool distanceFlag(float distance)
+{
     static float initialDistance = NAN;
     return distanceFlag(distance, &initialDistance);
 }
 
-bool orientationFlag(float angle) {
+bool orientationFlag(float angle)
+{
     static float initialOrientation = NAN;
     return orientationFlag(angle, &initialOrientation);
 }
 
-void rotate(float velocity, float radius) {
+void rotate(float velocity, float radius)
+{
     MOVE::WheelVelocities velocities = MOVE::moveByRadius(velocity, radius);
 
     rightPID.Sp = velocities.rightVelocity;
     leftPID.Sp = velocities.leftVelocity;
 }
 
-void move(float velocity, float angularVelocity) {
+void move(float velocity, float angularVelocity)
+{
     float rightWheelVelocity = velocity - (angularVelocity * WHEEL_BASE_DIAMETER) / 2.0;
     float leftWheelVelocity = velocity + (angularVelocity * WHEEL_BASE_DIAMETER) / 2.0;
 
@@ -179,13 +197,14 @@ void move(float velocity, float angularVelocity) {
     leftPID.Sp = leftWheelVelocity;
 }
 
-void moveUnited(float velocity, float radius, float orientation) {
+void moveUnited(float velocity, float radius, float orientation)
+{
 
     float baseAngularVelocity = isinf(radius) ? 0 : (velocity / radius);
 
-    //10 / 33 = 0.303
+    // 10 / 33 = 0.303
 
-    //Serial.println(radius);
+    // Serial.println(radius);
 
     float targetAngle = smallestAngleDifference(computeOrientation(), orientation);
     float angularVelocity = sigmoid(targetAngle, 0, 1, 1, -2) * -baseAngularVelocity;
@@ -193,11 +212,13 @@ void moveUnited(float velocity, float radius, float orientation) {
     move(velocity, angularVelocity);
 }
 
-bool rotate(float velocity, float radius, float angle) {
+bool rotate(float velocity, float radius, float angle)
+{
     static float initialOrientation = NAN;
     float actualOrientation = computeOrientation();
 
-    if (isnan(initialOrientation)) {
+    if (isnan(initialOrientation))
+    {
         initialOrientation = actualOrientation;
     }
 
@@ -205,50 +226,61 @@ bool rotate(float velocity, float radius, float angle) {
 
     bool angleReached = fabs(actualOrientation - initialOrientation) >= fabs(angle);
 
-    if (angleReached) {
+    if (angleReached)
+    {
         move(0, 0);
-    } else {
+    }
+    else
+    {
         moveUnited(velocity, fabs(radius), targetOrientation);
     }
-    
-    if (angleReached) {
+
+    if (angleReached)
+    {
         initialOrientation = NAN;
     }
 
     return false;
 }
 
-bool forward(float velocity, float distance) {
+bool forward(float velocity, float distance)
+{
     static float initialDistance = NAN;
     static float initialOrientation = NAN;
 
-    //float actualDistance = computeDistance();
-    //float actualOrientation = computeOrientation();
+    // float actualDistance = computeDistance();
+    // float actualOrientation = computeOrientation();
 
-    if (isnan(initialDistance)) {
+    if (isnan(initialDistance))
+    {
         initialOrientation = computeOrientation();
     }
 
     bool distanceReached = distanceFlag(distance, &initialDistance);
 
-    if (distanceReached) {
+    if (distanceReached)
+    {
         move(0, 0);
-    } else {
+    }
+    else
+    {
         moveUnited(velocity, velocity / 5.0, initialOrientation);
     }
 
-    //float correction = sigmoid(actualOrientation, initialOrientation, 1, 0.1, -2) * 5;
-    
-    //move(distanceReached ? 0 : velocity, 0);
+    // float correction = sigmoid(actualOrientation, initialOrientation, 1, 0.1, -2) * 5;
 
-    if (isnan(initialDistance)) {
-       initialOrientation = NAN;
+    // move(distanceReached ? 0 : velocity, 0);
+
+    if (isnan(initialDistance))
+    {
+        initialOrientation = NAN;
     }
 
     return distanceReached;
 }
 
-void followWall(float id, float velocity, float radius = 13.3, float distance = 425.0) {
+void followWall(float id, float velocity, float radius = 13.3, float distance = 425.0)
+{
     float baseAngularVelocity = velocity / radius;
 
     float angularVelocity = sigmoid(ROBUS_ReadIR(id), distance, 1, 25.0, -2) * (id == RIGHT ? baseAngularVelocity : -baseAngularVelocity);
@@ -256,12 +288,14 @@ void followWall(float id, float velocity, float radius = 13.3, float distance = 
     move(velocity, angularVelocity);
 }
 
-bool activateServoForDistance(float id, float distance, float targetAngle, float resetAngle) {
+bool activateServoForDistance(float id, float distance, float targetAngle, float resetAngle)
+{
     static float initialDistance = NAN;
 
     float actualDistance = computeDistance();
 
-    if (isnan(initialDistance)) {
+    if (isnan(initialDistance))
+    {
         initialDistance = actualDistance;
         SERVO_SetAngle(id, targetAngle);
     }
@@ -272,11 +306,13 @@ bool activateServoForDistance(float id, float distance, float targetAngle, float
     return distanceReached;
 }
 
-void setClaw(ClawState state) {
+void setClaw(ClawState state)
+{
     clawState = state;
 }
 
-void updatePIDs() {
+void updatePIDs()
+{
     rightPID.Pv = MOVE::computeLeftMotorSpeed();
     MOTOR_SetSpeed(RIGHT, clamp(rightPID.update(), -1, 1));
 
@@ -291,99 +327,231 @@ void loopLineFollower()
     leftPID.Sp = wheelVelocities.leftWheelSpeed;
 }
 
-void loop() {
+void loop()
+{
 
     delay(5);
 
     int closedTime = 19000;
     int openedTime = 1000;
 
-    if (millis() % (closedTime + openedTime) < closedTime) {
+    if (millis() % (closedTime + openedTime) < closedTime)
+    {
         setClaw(CLOSED);
-    } else {
+    }
+    else
+    {
         setClaw(OPENED);
     }
-    
-    
-    if (movementIndex == -1) { // tournant à droite
-        if (forward(10, 96 / 2.0)) {
+
+    if (movementIndex == -1)
+    { // tournant à droite
+        if (forward(10, 96 / 2.0))
+        {
             movementIndex++;
         }
-    } else if (movementIndex == 0) { // tournant à droite
-        if (rotate(10, 18 + 12 + 3, M_PI / 2.0)) {
+    }
+    else if (movementIndex == 0)
+    { // tournant à droite
+        if (rotate(10, 18 + 12 + 3, M_PI / 2.0))
+        {
             movementIndex++;
         }
-    } else if(movementIndex == 1) { //segment tapis
-        if (forward(10, 24)) {
+    }
+    else if (movementIndex == 1)
+    { // segment tapis
+        if (forward(10, 24))
+        {
             movementIndex++;
         }
-    } else if(movementIndex == 2) { // tournant à droite
-        if (rotate(10, 18 + 12 + 3, M_PI / 2.0)) {
+    }
+    else if (movementIndex == 2)
+    { // tournant à droite
+        if (rotate(10, 18 + 12 + 3, M_PI / 2.0))
+        {
             movementIndex++;
         }
-    } else if (movementIndex == 3) {
-        if (forward(10, 96)) {
+    }
+    else if (movementIndex == 3)
+    {
+        if (forward(10, 96))
+        {
             movementIndex = 0;
         }
     }
-    
 
-    if (ROBUS_ReadIR(RIGHT) > 500) {
+    if (ROBUS_ReadIR(RIGHT) > 500)
+    {
         armState = EXTENDED_RIGHT;
     }
-        
-        
-    if (ROBUS_ReadIR(LEFT) > 500) {
+
+    if (ROBUS_ReadIR(LEFT) > 500)
+    {
         armState = EXTENDED_LEFT;
     }
 
-    //loopLineFollower();
+    // loopLineFollower();
 
-    //move(30, 0);
+    // move(30, 0);
 
-    //Serial.println(armState);
+    // Serial.println(armState);
 
-    //SERVO_SetAngle(ARM_SERVO, 0);
+    // SERVO_SetAngle(ARM_SERVO, 0);
 
-    
     switch (armState)
     {
     case EXTENDED_RIGHT:
-        if (activateServoForDistance(ARM_SERVO, 85, 180, 40)) {
-           armState = NOT_EXTENDED;
+        if (activateServoForDistance(ARM_SERVO, 85, 180, 40))
+        {
+            armState = NOT_EXTENDED;
         }
         break;
-    
+
     case EXTENDED_LEFT:
-        if (activateServoForDistance(ARM_SERVO, 85, 0, 40)) {
+        if (activateServoForDistance(ARM_SERVO, 85, 0, 40))
+        {
             armState = NOT_EXTENDED;
         }
         break;
     }
-    
+
     SERVO_SetAngle(CLAW_SERVO, clawState == OPENED ? 75 : 110);
 
-   //followWall(RIGHT, velocity);
-   /*
-    float angularVelocity = 0;
-    float baseAngularVelocity = 1.2;
+    // followWall(RIGHT, velocity);
+    /*
+     float angularVelocity = 0;
+     float baseAngularVelocity = 1.2;
 
-    angularVelocity = sigmoid(ROBUS_ReadIR(0), 425.0, 1, 25.0, -2) * baseAngularVelocity;
+     angularVelocity = sigmoid(ROBUS_ReadIR(0), 425.0, 1, 25.0, -2) * baseAngularVelocity;
 
 
-    rotate(velocity, 16 / angularVelocity);
-    */
+     rotate(velocity, 16 / angularVelocity);
+     */
 
-   //ROBUS_ReadIR
-    
+    // ROBUS_ReadIR
 
-    //rightPID.Sp = 0;
-    //leftPID.Sp = 0;
+    // rightPID.Sp = 0;
+    // leftPID.Sp = 0;
 
-    //MOVE::WheelVelocities velocities = MOVE::moveByRadius(computeOrientation() < M_PI / 2 ? 16 : 0, 23.622);
+    // MOVE::WheelVelocities velocities = MOVE::moveByRadius(computeOrientation() < M_PI / 2 ? 16 : 0, 23.622);
 
-    //Serial.println((MOVE::averageSpeedD()+MOVE::averageSpeedD())/2.0);
-    //MOVE::updatePIDMain(5, MOVE::radiusToDV(9, -M_PI));
-    //PIDLigne::computeWheelSpeed();
+    // Serial.println((MOVE::averageSpeedD()+MOVE::averageSpeedD())/2.0);
+    // MOVE::updatePIDMain(5, MOVE::radiusToDV(9, -M_PI));
+    // PIDLigne::computeWheelSpeed();
+    updatePIDs();
+}
+
+void temploop()
+{
+    switch (state)
+    {
+    case 0: // Attente du sifflet, détection de la couleur de départ
+        /* code */
+        if (!Sifflet::active)
+        {
+            if (Sifflet::update(1.0))
+            {
+                Sifflet::trigger();
+            }
+        }
+        if (Sifflet::active)
+        {
+            if (/*Couleur départ == vert*/)
+            {
+                state = 1;
+            }
+            else if (/*Couleur départ == jaune*/)
+            {
+                state = 2;
+            }
+        }
+        break;
+
+    case 1: // Suivi du vert, détection du cup
+
+        /*Algo pour suivre la couleur verte*/
+
+        if (/*Détecte le cup à gauche*/)
+        {
+            //Sort le bras à gauche
+        }
+        if(/*Couleur sol est blanche*/)
+        {
+            //Remonte le bras
+            state = 3;
+        }
+
+        break;
+
+    case 2: // Suivi du jaune, détection du cup
+
+        /*Algo pour suivre la couleur jaune*/
+
+        if (/*Détecte le cup à droite*/)
+        {
+            //Sort le bras à droite
+        }
+        if(/*Couleur sol est blanche*/)
+        {
+            //Remonte le bras
+            state = 3;
+        }
+
+        break;
+
+    case 3: // Suivi de la ligne, détection de retour à la couleur
+
+        // Suit la ligne
+
+        if(/*Couleur du sol != blanc*/)
+        {
+            //Descend le cup
+            //Redresse le robot
+            state = 4;
+        }
+        break;
+
+    case 4: // On fait un tour et puis le shortcut
+        /* shortcut mode */
+
+        if(/*Détecte la ligne noire pour une deuxième fois*/)
+        {
+            state = 5;
+        }
+        break;
+
+    case 5: // Feni
+        /* Stop moteurs */
+        if(/*Pèse sur bumper arrière*/)
+        {
+            state = 0;// On restart le parcours
+        }
+        break;
+    }
+
+    updateEverything();
+}
+
+void updateEverything()
+{
+    switch (armState)
+    {
+    case EXTENDED_RIGHT:
+        if (activateServoForDistance(ARM_SERVO, 85, 180, 40))
+        {
+            armState = NOT_EXTENDED;
+        }
+        break;
+
+    case EXTENDED_LEFT:
+        if (activateServoForDistance(ARM_SERVO, 85, 0, 40))
+        {
+            armState = NOT_EXTENDED;
+        }
+        break;
+    }
+
+    SERVO_SetAngle(CLAW_SERVO, clawState == OPENED ? 75 : 110);
+
     updatePIDs();
 }
