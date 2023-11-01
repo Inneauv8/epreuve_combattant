86e7f5537f327d0446a3d93af2c56562d57023bc
Gabriel Grondin <gabriel.grondin@outlook.com>
2023-11-01 14:23:25 -0400

    test gabriel

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+// test pour push
+
+
 /**************************************************************************************************
 Nom du fichier : main.cpp
 Auteur : Maxime Boucher
@@ -65,9 +68,10 @@ enum ArmState
 
 ArmState armState = NOT_EXTENDED;
 ClawState clawState = OPENED;
-byte state = 2; //Remettre à zéro pour le sifflet 
+byte state = 2; // Remettre à zéro pour le sifflet
 
-void setupPID() {
+void setupPID()
+{
     setPIDRight(0.0425, 0.0001, 0.001);
     setPIDLeft(0.0425, 0.0001, 0.001);
 }
@@ -93,30 +97,32 @@ void setup()
     PIDLigne::initPID(2.7387791339, 2.625137795, 8, 0, 0.1, LINE_FOLLOWER_PINS, 45);
     Serial.begin(9600);
 
-
-    //CapteurLigne::initLine(LINE_FOLLOWER_PINS, 45);
+    // CapteurLigne::initLine(LINE_FOLLOWER_PINS, 45);
     ENCODER_Reset(RIGHT);
     ENCODER_Reset(LEFT);
-    //Sifflet::init();
+    // Sifflet::init();
 
     setClaw(CLOSED);
 }
 
-bool activateServoForDistance(float id, float distance, float targetAngle, float resetAngle) {
+bool activateServoForDistance(float id, float distance, float targetAngle, float resetAngle)
+{
     static float initialDistance = INACTIVE;
 
-    if (Movement::isInactive(initialDistance)) {
+    if (Movement::isInactive(initialDistance))
+    {
         SERVO_SetAngle(id, targetAngle);
     }
 
     bool distanceReached = Movement::distanceFlag(distance, &initialDistance);
-    
+
     SERVO_SetAngle(id, distanceReached ? resetAngle : targetAngle);
 
     return distanceReached;
 }
 
-void setArm(ArmState state) {
+void setArm(ArmState state)
+{
     armState = state;
 }
 
@@ -127,7 +133,8 @@ void loopLineFollower()
     setLeftSpeed(wheelVelocities.leftWheelSpeed);
 }
 
-void followWall(float id, float velocity, float radius = 13.3, float distance = 425.0) {
+void followWall(float id, float velocity, float radius = 13.3, float distance = 425.0)
+{
     float baseAngularVelocity = velocity / radius;
 
     float angularVelocity = sigmoid(ROBUS_ReadIR(id), distance, 1, 25.0, -2) * (id == RIGHT ? baseAngularVelocity : -baseAngularVelocity);
@@ -135,22 +142,25 @@ void followWall(float id, float velocity, float radius = 13.3, float distance = 
     Movement::move(velocity, angularVelocity);
 }
 
-void updateServos() {
+void updateServos()
+{
     switch (armState)
     {
     case EXTENDED_RIGHT:
-        if (activateServoForDistance(ARM_SERVO, 85, 180, 40)) {
-           armState = NOT_EXTENDED;
+        if (activateServoForDistance(ARM_SERVO, 85, 180, 40))
+        {
+            armState = NOT_EXTENDED;
         }
         break;
-    
+
     case EXTENDED_LEFT:
-        if (activateServoForDistance(ARM_SERVO, 85, 0, 40)) {
+        if (activateServoForDistance(ARM_SERVO, 85, 0, 40))
+        {
             armState = NOT_EXTENDED;
         }
         break;
     }
-    
+
     SERVO_SetAngle(CLAW_SERVO, clawState == OPENED ? 75 : 110);
 }
 
@@ -173,17 +183,16 @@ void updateEverything()
         break;
     }
 
-    //SERVO_SetAngle(CLAW_SERVO, clawState == OPENED ? 75 : 110);
+    // SERVO_SetAngle(CLAW_SERVO, clawState == OPENED ? 75 : 110);
     updateServos();
     updatePIDs();
 }
 
 void temploop()
 {
-    
+
     delay(5);
 
-    
     int closedTime = 19000;
     int openedTime = 1000;
 
@@ -196,68 +205,89 @@ void temploop()
         setClaw(OPENED);
     }
 
-    
     if (movementIndex == -1)
     { // tournant à droite
         if (forward(10, 96 / 2.0))
         {
             movementIndex++;
         }
-    } else if (movementIndex == 0) { // tournant à droite
-        if (rotate(20, 18 + 12, M_PI / 2.0)) {
+    }
+    else if (movementIndex == 0)
+    { // tournant à droite
+        if (rotate(20, 18 + 12, M_PI / 2.0))
+        {
             movementIndex++;
         }
-    } else if(movementIndex == 1) { //segment tapis
-        if (forward(20, 28)) {
+    }
+    else if (movementIndex == 1)
+    { // segment tapis
+        if (forward(20, 28))
+        {
             movementIndex++;
         }
-    } else if(movementIndex == 2) { // tournant à droite
-        if (rotate(20, 18 + 12 + 2, M_PI / 2.0 + M_PI / 8)) {
+    }
+    else if (movementIndex == 2)
+    { // tournant à droite
+        if (rotate(20, 18 + 12 + 2, M_PI / 2.0 + M_PI / 8))
+        {
             movementIndex++;
         }
-    } else if(movementIndex == 2) { // tournant à droite
-        if (forward(20, 96)) {
+    }
+    else if (movementIndex == 2)
+    { // tournant à droite
+        if (forward(20, 96))
+        {
             movementIndex++;
         }
-    } else if(movementIndex == 2) { // tournant à droite
-        if (rotate(20, 18 + 12, M_PI / 2.0)) {
+    }
+    else if (movementIndex == 2)
+    { // tournant à droite
+        if (rotate(20, 18 + 12, M_PI / 2.0))
+        {
             movementIndex++;
         }
-    } else if(movementIndex == 2) { // tournant à droite
-        if (forward(20, 24)) {
+    }
+    else if (movementIndex == 2)
+    { // tournant à droite
+        if (forward(20, 24))
+        {
             movementIndex++;
         }
-    } else if(movementIndex == 2) { // tournant à droite
-        if (rotate(20, 18 + 12, M_PI / 2.0)) {
+    }
+    else if (movementIndex == 2)
+    { // tournant à droite
+        if (rotate(20, 18 + 12, M_PI / 2.0))
+        {
             movementIndex++;
         }
-    }else if (movementIndex == 3) {
-        if (forward(20, 96)) {
+    }
+    else if (movementIndex == 3)
+    {
+        if (forward(20, 96))
+        {
             movementIndex = 0;
         }
     }
-    
-    
 
-    if (ROBUS_ReadIR(RIGHT) > 500) {
+    if (ROBUS_ReadIR(RIGHT) > 500)
+    {
         setArm(EXTENDED_RIGHT);
     }
-        
-    if (ROBUS_ReadIR(LEFT) > 500) {
+
+    if (ROBUS_ReadIR(LEFT) > 500)
+    {
         setArm(EXTENDED_LEFT);
     }
-    
 
-    //loopLineFollower();
-    //followWall(RIGHT, 10);
+    // loopLineFollower();
+    // followWall(RIGHT, 10);
 
     updatePIDs();
 }
 
-
 void loop()
 {
-   // Serial.println(state);
+    // Serial.println(state);
     delay(5);
     switch (state)
     {
@@ -285,67 +315,80 @@ void loop()
 
     case 1: // Suivi du vert, détection du cup
         static byte state2 = 0;
-        switch (state2) {
-            case 0:
-                moveUnited(5,INFINITY,0);
-                if (CapteurLigne::isBlackLine()) state2++;
+        switch (state2)
+        {
+        case 0:
+            moveUnited(5, INFINITY, 0);
+            if (CapteurLigne::isBlackLine())
+                state2++;
             break;
-            case 1:
-                if (rotate(15,18,PI/2.0)) state2++;
+        case 1:
+            if (rotate(15, 18, PI / 2.0))
+                state2++;
             break;
-            case 2:
-                if (forward(15,24)) state2++;
+        case 2:
+            if (forward(15, 24))
+                state2++;
             break;
-            case 3:
-                if (rotate(15,18,PI/2.0)) state2++;
+        case 3:
+            if (rotate(15, 18, PI / 2.0))
+                state2++;
             break;
-            case 4:
-                moveUnited(15,INFINITY,PI);
-                if (ROBUS_ReadIR(RIGHT) > 500)
-                        {
-                            setArm(EXTENDED_RIGHT);
-                        }
+        case 4:
+            moveUnited(15, INFINITY, PI);
+            if (ROBUS_ReadIR(RIGHT) > 500)
+            {
+                setArm(EXTENDED_RIGHT);
+            }
 
             break;
         }
-    break;
+        break;
 
     case 2: // Suivi du jaune, détection du cup
         static byte state3 = 0;
-        switch (state3) {
-            case 0:
-                moveUnited(5,INFINITY,0);
-                if (CapteurLigne::isBlackLine()) state3++;
+        switch (state3)
+        {
+        case 0:
+            moveUnited(5, INFINITY, 0);
+            if (CapteurLigne::isBlackLine())
+                state3++;
             break;
-            case 1:
-                if (rotate(15,33,PI/2.0)) state3++;
+        case 1:
+            if (rotate(15, 33, PI / 2.0))
+                state3++;
             break;
-            case 2:
-                if (forward(15,24)) state3++;
+        case 2:
+            if (forward(15, 24))
+                state3++;
             break;
-            case 3:
-                if (rotate(15,33,(PI/2.0)+0.1)) state3++;
+        case 3:
+            if (rotate(15, 33, (PI / 2.0) + 0.1))
+                state3++;
             break;
-            case 4:
-                if (forward(15,88.5)) state3++;
-                if (ROBUS_ReadIR(RIGHT) > 500)
-                        {
-                            setArm(EXTENDED_RIGHT);
-                        }
+        case 4:
+            if (forward(15, 88.5))
+                state3++;
+            if (ROBUS_ReadIR(RIGHT) > 500)
+            {
+                setArm(EXTENDED_RIGHT);
+            }
             break;
-            case 5:
-                if (rotate(10,3,PI/3)) state3++;
+        case 5:
+            if (rotate(10, 3, PI / 3))
+                state3++;
             break;
-            case 6:
-                if (forward(15,9)) state = 3;
+        case 6:
+            if (forward(15, 9))
+                state = 3;
             break;
         }
-    break;
+        break;
 
     case 3: // Suivi de la ligne, détection de retour à la couleur
 
         loopLineFollower();
-        if(Couleur::Get() == 'j' || Couleur::Get() == 'v')
+        if (Couleur::Get() == 'j' || Couleur::Get() == 'v')
         {
             setClaw(OPENED);
             rotate(10, 0, M_PI / 4.0);
@@ -355,10 +398,10 @@ void loop()
 
     case 4: // On fait un tour et puis le shortcut
     case 5:
-        
+
         followWall(RIGHT, 15);
 
-        if(CapteurLigne::isBlackLine())
+        if (CapteurLigne::isBlackLine())
         {
             state++;
         }
@@ -368,21 +411,21 @@ void loop()
 
         move(0, 0);
 
-        if(ROBUS_IsBumper(3))
+        if (ROBUS_IsBumper(3))
         {
             setClaw(CLOSED);
-            state = 0;// On restart le parcours
+            state = 0; // On restart le parcours
         }
         break;
     case 88:
         move(2, 0);
-    break;
+        break;
     case 89:
         Serial.println(CapteurLigne::isBlackLine());
-    break;
+        break;
     case 90:
         Serial.println(Couleur::Get());
-    break;
+        break;
     }
 
     updateEverything();
